653a059397eb6477f51ff47a2c6748ada89ad80e
Akshay-vaghasiya <akshayvaghasiya3636@gmail.com>
2024-12-27 12:16:46 +0530

    Modified StudentExamService and StudentAnswerRepository for implement auto-save, run code and submit code this features in backend

--- backend/src/main/java/com/example/backend/Repository/StudentAnswerRepository.java
+++ backend/src/main/java/com/example/backend/Repository/StudentAnswerRepository.java
@@ -1,8 +1,11 @@
 package com.example.backend.Repository;
 
 import com.example.backend.Entity.StudentAnswer;
+import com.example.backend.Entity.StudentExam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /* Repository interface for accessing and managing StudentAnswer entities in the database.
 
@@ -10,8 +13,16 @@ import org.springframework.stereotype.Repository;
    StudentAnswer entity. It allows seamless interaction with the underlying database, enabling operations such
    as saving, finding, updating, and deleting student answers to exam questions.
 
+ * Methods:
+   - findByStudentExamAndMcqQuestionId(StudentExam studentExam, Long mcqQuestionId): Retrieves a StudentAnswer entity based on
+     the specified studentExam and mcqQuestionId.
+   - findByStudentExamAndCodingQuestionId(StudentExam studentExam, Long codingQuestionId): Retrieves a StudentAnswer entity based on
+     the specified studentExam and codingQuestionId.
+
  * By extending JpaRepository, this interface also inherits various generic methods for data access and
    manipulation, such as save, findById, findAll, delete, etc. */
 @Repository
 public interface StudentAnswerRepository extends JpaRepository<StudentAnswer, Long> {
+    Optional<StudentAnswer> findByStudentExamAndMcqQuestionId(StudentExam studentExam, Long mcqQuestionId);
+    Optional<StudentAnswer> findByStudentExamAndCodingQuestionId(StudentExam studentExam, Long codingQuestionId);
 }
--- backend/src/main/java/com/example/backend/Service/StudentExamService.java
+++ backend/src/main/java/com/example/backend/Service/StudentExamService.java
@@ -1,19 +1,28 @@
 package com.example.backend.Service;
 
+import com.example.backend.Dto.AutoSaveRequest;
+import com.example.backend.Dto.StudentAnswerDTO;
 import com.example.backend.Dto.StudentExamDisplay;
 import com.example.backend.Entity.*;
 import com.example.backend.Repository.*;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,6 +43,40 @@ public class StudentExamService {
     // Injects an instance of ExamResultRepository for database operations
     @Autowired
     private ExamResultRepository examResultRepository;
+
+    // Injects an instance of StudentAnswerRepository for database operations
+    @Autowired
+    private StudentAnswerRepository studentAnswerRepository;
+
+    // Injects an instance of CodingQuestionRepository for database operations
+    @Autowired
+    private CodingQuestionRepository codingQuestionRepository;
+
+    // Injects an instance of McqQuestionRepository for database operations
+    @Autowired
+    private McqQuestionRepository mcqQuestionRepository;
+
+    // Data fields of this class
+    private final WebClient webClient;
+    private final ObjectMapper objectMapper;
+    Map<String, Integer> languagemap = new HashMap<>();
+
+    // Reads the judge0.api.key from environment variables
+    @Value("${judge0.api.key}")
+    private String apiKey;
+
+    // Create constructor with arguments webClientBuilder and objectMapper
+    @Autowired
+    public StudentExamService(WebClient.Builder webClientBuilder, ObjectMapper objectMapper) {
+        this.webClient = webClientBuilder.baseUrl("https://judge0-ce.p.rapidapi.com").build();
+        this.objectMapper = objectMapper;
+
+        languagemap.put("C++", 105);
+        languagemap.put("Java", 91);
+        languagemap.put("Javascript", 102);
+        languagemap.put("Python", 71);
+        languagemap.put("C", 103);
+    }
 
     /* Finds all eligible exams for a student based on their university, branch, and semester.
 
@@ -183,6 +226,224 @@ public class StudentExamService {
         examResultRepository.save(examResult);
 
         return "Successfully exam submited";
+    }
+
+    /* Saves student answers for a specific exam.
+
+   @param examId - The ID of the exam.
+   @param autoSaveRequest - The auto-save request containing answers to save.
+   @throws RuntimeException - If the StudentExam or Exam is not found.
+   @throws IllegalStateException - If the exam time is over. */
+    public void autoSaveAnswers(long examId, AutoSaveRequest autoSaveRequest) {
+
+        StudentExam studentExam = studentExamRepository.findById(autoSaveRequest.getStudentExamId())
+                .orElseThrow(() -> new RuntimeException("StudentExam not found"));
+
+        Exam exam = examRepository.findById(examId)
+                .orElseThrow(() -> new RuntimeException("Exam not found"));
+
+        String duration = exam.getDuration();
+        int durationint = Integer.parseInt(duration);
+
+        LocalDateTime starttime = studentExam.getStartTime();
+        LocalDateTime endtime = studentExam.getEndTime();
+
+        if (durationint < Duration.between(starttime, endtime).toMinutes()) {
+            throw new IllegalStateException("Auto-save failed because of Exam time is over.");
+        }
+
+        for (StudentAnswerDTO answerDTO : autoSaveRequest.getAnswers()) {
+            StudentAnswer studentAnswer = getOrCreateStudentAnswer(studentExam, answerDTO);
+            studentAnswer.setAnswer(answerDTO.getAnswer());
+            studentAnswer.setLanguage(answerDTO.getLanguage());
+
+            if (answerDTO.getQuestionType().equalsIgnoreCase("mcq")) {
+
+                List<McqOption> answer = (studentAnswer.getMcqQuestion().getOptions().stream().filter(ans -> ans.getisCorrect())).collect(Collectors.toList());
+                if(answer.get(0).getOptionText().equalsIgnoreCase(answerDTO.getAnswer())) {
+                    studentAnswer.setCorrect(true);
+                } else {
+                    studentAnswer.setCorrect(false);
+                }
+
+            }
+
+            studentAnswerRepository.save(studentAnswer);
+        }
+
+        studentExam.setEndTime(LocalDateTime.now());
+        studentExamRepository.save(studentExam);
+    }
+
+    /* Retrieves or creates a StudentAnswer record for the provided question and student exam.
+
+       @param studentExam - The student exam to associate the answer with.
+       @param answerDTO - The DTO containing answer data.
+       @return The retrieved or newly created StudentAnswer. */
+    private StudentAnswer getOrCreateStudentAnswer(StudentExam studentExam, StudentAnswerDTO answerDTO) {
+
+        if (answerDTO.getQuestionType().equalsIgnoreCase("mcq")) {
+            return studentAnswerRepository.findByStudentExamAndMcqQuestionId(studentExam, answerDTO.getQuestionId())
+                    .orElseGet(() -> {
+                        StudentAnswer newAnswer = new StudentAnswer();
+                        newAnswer.setStudentExam(studentExam);
+                        McqQuestion mcqQuestion = mcqQuestionRepository.findById(answerDTO.getQuestionId())
+                                .orElseThrow(() -> new RuntimeException("MCQ Question not found"));
+                        newAnswer.setMcqQuestion(mcqQuestion);
+                        return newAnswer;
+                    });
+        } else if (answerDTO.getQuestionType().equalsIgnoreCase("coding")) {
+            return studentAnswerRepository.findByStudentExamAndCodingQuestionId(studentExam, answerDTO.getQuestionId())
+                    .orElseGet(() -> {
+                        StudentAnswer newAnswer = new StudentAnswer();
+                        newAnswer.setStudentExam(studentExam);
+                        CodingQuestion codingQuestion = codingQuestionRepository.findById(answerDTO.getQuestionId())
+                                .orElseThrow(() -> new RuntimeException("Coding Question not found"));
+                        newAnswer.setCodingQuestion(codingQuestion);
+                        return newAnswer;
+                    });
+        } else {
+            throw new IllegalArgumentException("Invalid question type: " + answerDTO.getQuestionType());
+        }
+
+    }
+
+    /* Executes a given source code on the Judge0 API and retrieves the result.
+
+       @param sourceCode - The source code to execute.
+       @param languageId - The ID of the programming language.
+       @param stdin - The input for the program.
+       @return The result of the code execution.
+       @throws IllegalArgumentException - If the source code or language ID is invalid.
+       @throws RuntimeException - If the API call or response processing fails. */
+    public Object runCode(String sourceCode, int languageId, String stdin) {
+        if (sourceCode == null || sourceCode.trim().isEmpty()) {
+            throw new IllegalArgumentException("Source code cannot be null or empty");
+        }
+
+        if (languageId <= 0) {
+            throw new IllegalArgumentException("Invalid language ID: " + languageId);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-RapidAPI-Host", "judge0-ce.p.rapidapi.com");
+        headers.set("X-RapidAPI-Key", apiKey);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("source_code", sourceCode);
+        body.put("language_id", languageId);
+        body.put("stdin", stdin);
+
+        try {
+
+            String response = webClient.post()
+                    .uri("/submissions?base64_encoded=false&wait=true&field=*")
+                    .headers(httpHeaders -> httpHeaders.putAll(headers))
+                    .bodyValue(body)
+                    .exchangeToMono(clientResponse -> {
+                        if (clientResponse.statusCode().is2xxSuccessful()) {
+                            return clientResponse.bodyToMono(String.class);
+                        } else {
+                            return clientResponse.bodyToMono(String.class)
+                                    .flatMap(errorBody -> Mono.error(new RuntimeException("API call failed " + errorBody)));
+                        }
+                    })
+                    .block();
+
+            return objectMapper.readValue(response, new TypeReference<Map<String, Object>>() {});
+        } catch (WebClientResponseException e) {
+            System.err.println("API call failed " + e.getResponseBodyAsString());
+            throw new RuntimeException("API call failed " + e.getResponseBodyAsString());
+        } catch (Exception e) {
+            System.err.println("Error processing response " + e.getMessage());
+            throw new RuntimeException("Error processing response " + e.getMessage(), e);
+        }
+    }
+
+    /* Executes a given answer code on the Judge0 API and retrieves the result and submit that if output is correct.
+
+       @param email - The email of the student.
+       @param examId - The ID of the exam.
+       @param studentAnswerDTO - A DTO containing the answers of code.
+       @return string message based code output.
+       @throws IllegalArgumentException - If the student code or language is invalid.
+       @throws EntityNotFoundException - If the Exam or CodingQuestion is not found. */
+    public String submitCode(String email, long examId, StudentAnswerDTO studentAnswerDTO) {
+
+        if (studentAnswerDTO.getAnswer() == null || studentAnswerDTO.getAnswer().trim().isEmpty()) {
+            throw new IllegalArgumentException("Source code cannot be null or empty");
+        }
+
+        if ((studentAnswerDTO.getLanguage() == null) || studentAnswerDTO.getLanguage().trim().isEmpty() || (languagemap.get(studentAnswerDTO.getLanguage()) == null)) {
+            throw new IllegalArgumentException("Invalid language " + studentAnswerDTO.getLanguage());
+        }
+
+        StudentExam studentExam = studentExamRepository.findByStudentEmailAndExamId(email, examId).orElseThrow(
+                () -> new EntityNotFoundException("studentExam not found")
+        );
+
+        Exam exam = examRepository.findById(examId)
+                .orElseThrow(() -> new EntityNotFoundException("Exam not found"));
+
+        int durationint = Integer.parseInt(exam.getDuration());
+        LocalDateTime starttime = studentExam.getStartTime();
+        LocalDateTime endtime = studentExam.getEndTime();
+
+        if (durationint < Duration.between(starttime, endtime).toMinutes() && studentExam.isCompleted()) {
+            throw new IllegalStateException("Code submission failed because of Exam time is over.");
+        }
+
+        CodingQuestion codingQuestion = codingQuestionRepository.findById(studentAnswerDTO.getQuestionId()).orElseThrow(
+                () -> new EntityNotFoundException("Coding Question not found")
+        );
+
+        Set<CodingTestCase> codingTestCases = codingQuestion.getTestCases();
+
+        String stdin = codingTestCases.size() + "\n";
+        for (CodingTestCase codingTestCase : codingTestCases) {
+            stdin += codingTestCase.getInputData() + "\n";
+        }
+
+        Map<String, Object> response = (Map<String, Object>) runCode(studentAnswerDTO.getAnswer(), languagemap.get(studentAnswerDTO.getLanguage()), stdin);
+
+        if (response.get("stderr") != null || response.get("error") != null) {
+            return "Your code gives an error.";
+        }
+
+        String output = "";
+        for (CodingTestCase codingTestCase : codingTestCases) {
+            output += codingTestCase.getExpectedOutput();
+            if (!codingTestCase.getExpectedOutput().equals("")) {
+                output += " ";
+            }
+            output += "\n";
+        }
+
+        if (response.get("stdout") != null && response.get("stdout").equals(output)) {
+            StudentAnswer answer = studentAnswerRepository.findByStudentExamAndCodingQuestionId(studentExam, studentAnswerDTO.getQuestionId())
+                    .orElseGet(() -> {
+                        StudentAnswer newAnswer = new StudentAnswer();
+                        newAnswer.setStudentExam(studentExam);
+                        CodingQuestion codingQuestion1 = codingQuestionRepository.findById(studentAnswerDTO.getQuestionId())
+                                .orElseThrow(() -> new RuntimeException("Coding Question not found"));
+                        newAnswer.setCodingQuestion(codingQuestion1);
+                        return newAnswer;
+                    });
+
+            answer.setCorrect(true);
+            answer.setStudentExam(studentExam);
+            answer.setAnswer(studentAnswerDTO.getAnswer());
+            answer.setLanguage(studentAnswerDTO.getLanguage());
+
+            studentAnswerRepository.save(answer);
+            studentExam.setEndTime(LocalDateTime.now());
+            studentExamRepository.save(studentExam);
+
+            return "Your code is correct and submitted successfully";
+        } else {
+            return "Output does not match the expected output. Your output: " + response.get("stdout");
+        }
     }
 
 }
